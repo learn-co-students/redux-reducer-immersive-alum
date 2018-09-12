fbe2f26e1c79a613197341133ae81de7a677ca1b
ethanryan <ethanryan@gmail.com>
2018-09-12 15:37:40 -0400

    updated managePresents reducer with 'INCREASE' case

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,5 +1,10 @@
 export function managePresents(state, action){
   switch(action.type) {
+    case 'INCREASE':
+    return {
+      ...state,
+      numberOfPresents: state.numberOfPresents + 1,
+    }
     default:
     return state
   }
