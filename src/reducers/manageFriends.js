9663f1956e57a83edd987e51479adb0a5f9d79b9
ethanryan <ethanryan@gmail.com>
2018-09-12 15:59:03 -0400

    updated manageFriends reducer with 'ADD_FRIEND' case

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,5 +1,9 @@
 export function manageFriends(state, action){
   switch(action.type) {
+    case 'ADD_FRIEND':
+    return {
+      friends: [...state.friends, action.friend]
+    }
     default:
     return state
   }
