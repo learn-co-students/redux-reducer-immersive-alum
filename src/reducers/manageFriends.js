b868abd6e298a087e72b782d6fdb9cafb5fb5cfb
ethanryan <ethanryan@gmail.com>
2018-09-12 16:07:39 -0400

    updated manageFriends reducer with 'REMOVE_FRIEND' case, using filter function based on friend.id

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,7 +2,15 @@ export function manageFriends(state, action){
   switch(action.type) {
     case 'ADD_FRIEND':
     return {
-      friends: [...state.friends, action.friend]
+      friends: [
+        ...state.friends, action.friend
+      ]
+    }
+    case 'REMOVE_FRIEND':
+    return {
+      friends: [
+        ...state.friends.filter(friend => friend.id !== action.id)
+      ]
     }
     default:
     return state
