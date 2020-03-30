4f4464bf15d109e2d2e677bb25dca256f6772fa5
Ned-el-ch <nedelchev.niki@gmail.com>
2020-03-30 11:49:09 +0100

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+	switch (action.type) {
+		case "ADD_FRIEND":
+			return {friends: [...state.friends, action.friend]}
+		case "REMOVE_FRIEND":
+			let arr = state.friends.filter(e => {
+				if (e.id != action.id) {
+					return e;
+				}
+			})
+			return {friends: arr}
+		default:
+			return state;
+	}
 }
